45c7bfcf883b19dcb9bf2c100aa598243d253813
alexkinsey <alexanderkinsey@icloud.com>
2021-03-30 16:06:18 +0100

    initial commit

---
+++ specs/room_spec.js
@@ -0,0 +1,30 @@
+const assert = require('assert');
+const oom = require()'../room.js';
+
+describe('Room', function() {
+    
+    let room;
+    beforeEach(function() {
+        room = new Room('Palace', 100);
+    })
+
+    it('should have a name', function() {
+        const result = room.name;
+        assert.strictEqual(result, 'Palace');
+    })
+
+    it('should have size', function() {
+        const result = room.size;
+        assert.strictEqual(result, 100);
+    })
+
+    it('should start not painted', function() {
+        const result = room.painted;
+        assert.strictEqual(result, false);
+    })
+
+    xit('should be able to be painted', function() {
+        const result 
+    })
+
+})
